2a35fe7975db53bf3f13cfa747cd3eff14b60e05
LouisIV <Louislombardoiv@gmail.com>
2019-05-05 11:36:43 -0700

    Finished hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-70b866c4c0668200.d
@@ -0,0 +1,5 @@
+/Users/louislombardo/Projects/learning-rust/hello_cargo/target/debug/deps/hello_cargo-70b866c4c0668200.rmeta: src/main.rs
+
+/Users/louislombardo/Projects/learning-rust/hello_cargo/target/debug/deps/hello_cargo-70b866c4c0668200.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-9e6571a33c3382ad.d
@@ -0,0 +1,5 @@
+/Users/louislombardo/Projects/learning-rust/hello_cargo/target/debug/deps/hello_cargo-9e6571a33c3382ad: src/main.rs
+
+/Users/louislombardo/Projects/learning-rust/hello_cargo/target/debug/deps/hello_cargo-9e6571a33c3382ad.d: src/main.rs
+
+src/main.rs:
